414bcffd8c59e806727cf0848515f82324e9b0ba
GRady15 <jimmyrady15@gmail.com>
2024-12-14 17:22:04 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,9 @@
+let img = document.querySelector('.img');
+let container = document.querySelector('.container');
+
+function phones(mobile){
+    img.src = mobile;
+}
+function colors(color){
+    container.style.background = color; 
+}
